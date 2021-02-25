710dbb683a29f2a74b9a230a656897ba36a5b497
Sergiy Ensary <sergiyensary@gmail.com>
2021-02-24 20:54:20 -0600

    Chapter 8 exercise 5 and 6

---
+++ Chapter-08/dirent.h
@@ -0,0 +1,22 @@
+#ifndef DIRENT_H
+#define DIRENT_H
+#endif
+
+#define NAME_MAX 14
+
+typedef struct {
+	long ino;
+	char name[NAME_MAX+1];
+} Dirent;
+
+typedef struct {
+	int fd;
+	Dirent d;
+} DIR;
+
+DIR *opendir(char *dirname);
+Dirent *readdir(DIR *dfd);
+void closedir(DIR *dfd);
+
+char *name;
+struct stat stbuf;
